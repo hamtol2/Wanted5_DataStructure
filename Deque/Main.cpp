f386df2e986ce77e65e8a2e635a8cf3022f8ff59
ronniej <ronniej@naver.com>
2026-08-21 12:00:11 +0900

    덱 구현 및 실습 진행

---
+++ Deque/Main.cpp
@@ -0,0 +1,23 @@
+﻿#include "Deque.h"
+#include <iostream>
+#include <queue>
+#include <stack>
+
+int main()
+{
+	Deque<int> deque;
+
+	for (int ix = 1; ix <= 10; ++ix)
+	{
+		if (ix % 2 == 0)
+		{
+			deque.AddFront(ix);
+		}
+		else
+		{
+			deque.AddRear(ix);
+		}
+	}
+
+	deque.Print();
+}
