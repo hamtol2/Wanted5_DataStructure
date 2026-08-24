a1b165c18cbe0e5f347d73d55fbc2f1fd6610e51
ronniej <ronniej@naver.com>
2026-08-24 10:55:58 +0900

    다중 트리 구현(순회는 구현해야함)

---
+++ Tree/Main.cpp
@@ -0,0 +1,29 @@
+﻿#include "Tree.h"
+#include <iostream>
+#include <string>
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	// 트리 생성.
+	Tree<std::string> tree("A");
+
+	// 자손 노드 추가.
+	tree.AddChild("A", "B");
+	tree.AddChild("A", "C");
+	tree.AddChild("A", "D");
+
+	tree.AddChild("B", "E");
+	tree.AddChild("B", "F");
+	tree.AddChild("B", "G");
+
+	tree.AddChild("C", "H");
+
+	tree.AddChild("D", "I");
+	tree.AddChild("D", "J");
+}
